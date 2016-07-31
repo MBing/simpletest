3683cef2366f3ac8cce5ed91334797b0d17b8a4f
Martin Bing <bing.martin.photography@gmail.com>
2016-07-31 12:53:14 +0200

    Add super and refactor to make msg nav item work

--- src/MessageNavItem.jsx
+++ src/MessageNavItem.jsx
@@ -1,8 +1,13 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router';
+import header from './styles';
+import MessagesData from './messagesData';
 
 export default class Message extends Component {
     constructor (props) {
+        super(props);
         console.log(props.message);
+        this.message = props.message;
     }
     deleteMessage (id) {
         MessagesData.deleteMessage(id);
@@ -11,10 +16,10 @@ export default class Message extends Component {
     render() {
         // let data = MessagesData.getMessage(this.props.params.uid);
         return (
-            <li key={ message.uid }>
-                <Link to={`/messages/${message.uid}`} activeStyle={ header.isActive }>{message.sender}</Link>
+            <li key={ this.message.uid }>
+                <Link to={`/messages/${this.message.uid}`} activeStyle={ header.isActive }>{this.message.sender}</Link>
                 {/*<Link to={`/messages/delete/${message.uid}`}> X </Link>*/}
-                <button onClick={ this.deleteMessage.bind(message.uid) }>X</button>
+                <button onClick={ this.deleteMessage.bind(this.message.uid) }>X</button>
             </li>
         );
     }
--- src/Messages.jsx
+++ src/Messages.jsx
@@ -1,26 +1,12 @@
 import React, { Component } from 'react';
-import { Link } from 'react-router';
-import header from './styles';
 import MessageNavItem from './MessageNavItem';
 import MessagesData from './messagesData';
 
 export default class Messages extends Component {
-    // getInitialState () {
-    //     return { messageData: this.props.initialData };
-    // }
-    // deleteMessage (id) {
-    //     MessagesData.deleteMessage(id);
-    //     this.setState({ messageData: MessagesData.getAll() });
-    // }
     render() {
-        this.messages = messageData.map(function (message) {
+        this.messages = MessagesData.getAll().map(function (message) {
             return (
-                <MessageNavItem message={message} />
-                {/*<li key={ message.uid }>*/}
-                    {/*<Link to={`/messages/${message.uid}`} activeStyle={ header.isActive }>{message.sender}</Link>*/}
-                    {/*/!*<Link to={`/messages/delete/${message.uid}`}> X </Link>*!/*/}
-                    {/*<button onClick={ this.deleteMessage.bind(message.uid) }>X</button>*/}
-                {/*</li>*/}
+                <MessageNavItem message={message} key={message.uid} />
             )
         });
         return (

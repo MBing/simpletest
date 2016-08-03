8b6046f535dbfc6a684a7ab876ab44c81d106edc
Martin Bing <bing.martin.photography@gmail.com>
2016-08-03 15:34:19 +0200

    set up test env for message

---
+++ test/Message.spec.js
@@ -0,0 +1,16 @@
+/**
+ * Created by Martin on 03/08/16.
+ */
+import React from 'react';
+import ReactTestUtils, { createRenderer } from 'react-addons-test-utils';
+import { should } from 'chai';
+import Message from '../src/Message';
+
+describe('Message component', function() {
+    it('Renders HTML', function() {
+        createRenderer().render(<Message />);
+        var result = createRenderer().getRenderOutput();
+        console.log(result, "<<< result");
+        result.type.should.equal(List);
+    });
+});
